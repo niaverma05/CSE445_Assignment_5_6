ab276f23fb7743b110a808cb1fe37cfb08179932
dtorre61 <dtorre61@asu.edu>
2026-04-20 16:50:03 -0700

    Add files via upload

---
+++ UnitConverterService/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitConverterService")]
+[assembly: AssemblyDescription("Unit Converter WCF Service for StudentHelperApp")]
+[assembly: AssemblyCompany("ASU CSE445")]
+[assembly: AssemblyProduct("UnitConverterService")]
+[assembly: AssemblyCopyright("Copyright 2025")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a1b2c3d4-e5f6-7890-abcd-ef1234567890")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
