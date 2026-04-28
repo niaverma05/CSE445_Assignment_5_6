75580c5a704b7e95166604e719a5201a8310ae70
dtorre61 <dtorre61@asu.edu>
2026-04-27 19:37:53 -0700

    Update Member.aspx.designer.cs

--- StudentHelperApp/Member.aspx.designer.cs
+++ StudentHelperApp/Member.aspx.designer.cs
@@ -9,18 +9,12 @@
 
 namespace StudentHelperApp
 {
-
-
     public partial class Member
     {
-
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.WebControls.Label lblUsername;
+        protected global::System.Web.UI.WebControls.Button btnLogout;
+        protected global::System.Web.UI.WebControls.Label lblMemberCookieStatus;
+        protected global::System.Web.UI.WebControls.Label lblSessionInfo;
+        protected global::System.Web.UI.WebControls.Label lblSessionId;
     }
 }
